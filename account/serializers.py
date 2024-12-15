99a6972ffcf376eb2e30f8301c7188e7081b0f93
Samiiz <skboss159@gmail.com>
2024-12-16 01:16:23 +0900

    serversttings

--- account/serializers.py
+++ account/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Product, User, Order
+from .models import Order, Product, User
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -8,11 +8,11 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = "__all__"
 
     def create(self, validated_data):
         user = User(**validated_data)
-        user.set_password(validated_data['password'])
+        user.set_password(validated_data["password"])
         user.save()
         return user
 
@@ -20,15 +20,16 @@ class UserSerializer(serializers.ModelSerializer):
 class UserGetSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'name', 'affiliation', 'address', 'api')
+        fields = ("id", "email", "name", "affiliation", "address", "api")
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = "__all__"
+
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = "__all__"
--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import User
-from .serializers import UserSerializer, UserGetSerializer
+from .serializers import UserGetSerializer, UserSerializer
 
 
 class UserView(APIView):
